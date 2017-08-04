5abe5ef430d66cfceb5010e047ac1ed10617cb6c
Bansi Bhatt <bannsi.bhatt07@gmail.com>
2017-08-04 11:07:19 +0530

    Initial Commit

---
+++ CAGradientLayerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CAGradientLayerDemo
+//
+//  Created by Zeitech Solutions on 04/08/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
